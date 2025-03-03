8f50ee52fc94a10806a130b5ed80aea9cac322a1
Mateus <p_mateus.schultz@b3digitas.com>
2025-03-03 17:15:12 -0300

    feat: include about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { About } from "@/components/About";
 
 export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
       <Header />
 
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <Hero />
+      </section>
+
+      <section id="about" className="snap-center">
+        <About />
       </section>
     </div>
   );
---
+++ src/components/About.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export const About = () => {
+  return (
+    <motion.div
+      className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        src="https://github.com/mateus-schultz.png"
+        alt="Mateus Schultz"
+        className="mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+        initial={{ x: -200, opacity: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, x: 0 }}
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">Here is a little background</h4>
+        <p className="text-base">
+          Hello! My name is Mateus Schultz, I am a Software Engineer with a
+          passion for creating innovative and user-friendly software solutions.
+          With a strong background in web development, I have honed my skills in
+          HTML, CSS, JavaScript, and React, enabling me to build dynamic and
+          interactive web applications.
+        </p>
+      </div>
+    </motion.div>
+  );
+};
