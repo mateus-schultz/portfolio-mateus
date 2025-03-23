4b65feda5b0b100a439c069f46294973926cdd6b
Mateus <p_mateus.schultz@b3digitas.com>
2025-03-23 13:10:48 -0300

    feat: increment skills

--- src/components/Sections/Skills.tsx
+++ src/components/Sections/Skills.tsx
@@ -49,33 +49,126 @@ export const Skills = () => {
         },
         {
           id: 2,
-          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
-          title: "Next.js",
-        },
-        {
-          id: 3,
           url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
           title: "Angular",
         },
         {
-          id: 4,
+          id: 3,
           url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
           title: "Vue",
         },
         {
+          id: 4,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+          title: "Next.js",
+        },
+        {
           id: 5,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vitejs/vitejs-original.svg",
+          title: "Vite",
+        },
+        {
+          id: 6,
           url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
           title: "Tailwind",
         },
         {
-          id: 6,
+          id: 7,
           url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
           title: "Material UI",
+        },
+        {
+          id: 8,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+          title: "Bootstrap",
+        },
+        {
+          id: 9,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
+          title: "Sass",
+        },
+        {
+          id: 10,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
+          title: "Redux",
+        },
+        {
+          id: 11,
+          url: "https://zustand-demo.pmnd.rs/logo192.png",
+          title: "Zustand",
         },
       ],
     },
     {
       id: 3,
+      group: "Mobile Frameworks and Tools",
+      skills: [
+        {
+          id: 1,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          title: "React Native",
+        },
+        {
+          id: 2,
+          url: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAOVBMVEVHcEwAABQAABQAABQAABQAABQAABMAABQAABMAABQAAACkpKaQkJPDw8T+/v5SUlfg4OF1dXkvLzZWMrGaAAAACnRSTlMAPJLF5PX/xAPaQKz+0wAAAKxJREFUeAF904UBhDAMAMBQd9t/2CeP1YNzdYEzDkIZF1VwJsmBAkqLaWh1ItpcAQ6xjAPIGgnINVJg9acx9ReDpg/WNT2COp8Pwdd5KzQxnBHNHBNimqLJ4R/ZzBDLxJInaBy2BtvkzIiY53l0aOxVG9ZsTY/YzqfNHWLGcmHBrC265w+mcw2iGjG8vgP/2vfKgYllMKBrlPtlsl1g26W5X9S4HWS/Heh/O/wA4LAUsuFVSoYAAAAASUVORK5CYII=",
+          title: "Expo",
+        },
+        {
+          id: 3,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg",
+          title: "Flutter",
+        },
+        {
+          id: 4,
+          url: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcBAMAAACAI8KnAAAAMFBMVEVHcExOOfpOOflOOflOOflOOflOOflOOflOOflOOflPOfpOOflOOfpOOflOOflOOflDUmf/AAAAEHRSTlMAEsnmtO7/1HuIHmgwRpeki/D/EgAAAORJREFUeAFEzyFMQlEUgOEPJ4j4mBbtww2rm5P8En1zMwPKdTwYmLCS3GwmJk0ryUQ2mUlkLWz2PtzbnfKXk87Od1A4vfgrRbFTq4Va3tktHidMX8EVhS5KKRz02Blgtw2lFqsUlSE0lwRQR9Kh3AerCcU2h4N53nF7/rZ+phr+60JjlvcSnmYTWLyD6ZJ4LI57oHz3kDfOgEKI3+T83BcivyEC6ilUR8D1lo/9fjKEdbx3cm6Myg3Q/JDz9zLgkmKLxZeICzHibu+nPtxsvomKLAlHACSjzwwAxhHpd1CwvAqJAwCHaUPtIQFi4AAAAABJRU5ErkJggg==",
+          title: "FlutterFlow",
+        },
+      ],
+    },
+    {
+      id: 4,
+      group: "Test Frameworks and Tools",
+      skills: [
+        {
+          id: 1,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
+          title: "Jest",
+        },
+        {
+          id: 2,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypressio/cypressio-original.svg",
+          title: "Cypress",
+        },
+        {
+          id: 3,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/playwright/playwright-original.svg",
+          title: "Playwright",
+        },
+        {
+          id: 4,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitest/vitest-original.svg",
+          title: "Vitest",
+        },
+      ],
+    },
+    {
+      id: 5,
+      group: "Monorepo Frameworks",
+      skills: [
+        {
+          id: 1,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original.svg",
+          title: "Yarn",
+        },
+        {
+          id: 2,
+          url: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcBAMAAACAI8KnAAAAMFBMVEVHcEzL1eHL1eHL1eHL1eHL1eHL1eHL1uHL1eHL1eHL1eHL1eHL1eHL1eHL1eHL1eEEq73kAAAAEHRSTlMAMmljRIH/GnO30uylmQ/EUCCQwAAAALtJREFUeAGtzzEIQWEUxfEjkpXJbHjZDPZiUfayAv09pKw2C7NJmeyDMlmUTIoMBpNBBslon3y3V7A7w61f99bp6o9JZHPBCFKiKY0Yv9mWvC+S+9lSkbfafNhV/3BUeHcKSK9ILQSsjUueQygAXB3LNLwRcNjydLzQ8WZQn85pOh7dvQ+nW2ZfN0ahvWUgJX1jBGrWLuWNmrCHlmNcxjvWbk/JGPPp3FkE3EhnqjG6xkTKHT3cSKX117wA4IBOhWXLrx4AAAAASUVORK5CYII=",
+          title: "Nx",
+        },
+      ],
+    },
+    {
+      id: 6,
       group: "Backend Frameworks and Tools",
       skills: [
         {
@@ -96,13 +189,13 @@ export const Skills = () => {
       ],
     },
     {
-      id: 4,
+      id: 7,
       group: "Build Tools and Deployment",
       skills: [
         {
           id: 1,
-          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vitejs/vitejs-original.svg",
-          title: "Vite",
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg",
+          title: "Webpack",
         },
         {
           id: 2,
@@ -114,10 +207,35 @@ export const Skills = () => {
           url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
           title: "Git",
         },
+        {
+          id: 4,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+          title: "GitHub",
+        },
+        {
+          id: 5,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg",
+          title: "Gitlab",
+        },
+        {
+          id: 6,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bitbucket/bitbucket-original.svg",
+          title: "BitBucket",
+        },
+        {
+          id: 7,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg",
+          title: "Vercel",
+        },
+        {
+          id: 8,
+          url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+          title: "Figma",
+        },
       ],
     },
     {
-      id: 5,
+      id: 8,
       group: "Other Utilities and Libraries",
       skills: [
         {
